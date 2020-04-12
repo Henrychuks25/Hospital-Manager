0d04fdb0bc937dfae3ce037a3e913771872eff1f
Henrychuks <henrichukwudi@gmail.com>
2020-04-12 06:01:52 +0100

    my first push of empty file

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hospital_Manager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
